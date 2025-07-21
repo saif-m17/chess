ae248f5cd582c67ac7929dce8c7b1031ac55b6d2
Saif Moolji <smoolji@berkeley.edu>
2025-07-20 23:37:01 -0400

    time benchmarking for standard starting position through depth 7

---
+++ move_gen/src/bin/perft_bench.rs
@@ -0,0 +1,16 @@
+use chess_core::board::Board;
+use chess_core::movegen::{*};
+use chess_core::moves::{*}; 
+use chess_core::utils::{*}; 
+use std::time::Instant;
+
+fn main() {
+
+    let mut board = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap();
+    for d in 1..=7 {
+        let now = Instant::now();
+        let n = perft(&mut board, d, Color::White);
+        println!("Depth {d}: {n} nodes in {:?}", now.elapsed());
+    }
+
+}
